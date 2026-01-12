30955ecdf1e94b94202abb52eb2d93453fb0ce9c
Andrew Conniff <anconnif@microsoft.com>
2026-01-12 19:24:18 +0000

    Refactor code structure for improved readability and maintainability

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GH_300Agent_too.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
